972b0e2efeebe68face0768815de4edb41fd54c9
Stulken <Arne.Stulken@btc-ag.com>
2016-09-28 16:41:06 +0200

    Signed-off-by: Stulken <Arne.Stulken@btc-ag.com>

--- app/src/main/java/arstulke/projectbook/BooksActivity.java
+++ app/src/main/java/arstulke/projectbook/BooksActivity.java
@@ -1,9 +1,15 @@
 package arstulke.projectbook;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -15,15 +21,13 @@ public class BooksActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+        setColors();
 
         Intent intent = getIntent();
         String library = intent.getStringExtra("library");
-
         setTitle(getString(R.string.app_name) + ": Bücherliste(" + library + ")");
-
-        String[] values = new String[] { "Cucumber", "Think Java", "Grundkurs Programmieren in Java"};
+        String[] values = new String[] { "Cucumber", "Think Java", "Think Java", "Think Java", "Think Java", "Think Java", "Think Java", "Think Java", "Grundkurs Programmieren in Java"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
         listView = (ListView) findViewById(R.id.booksView);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -35,5 +39,19 @@ public class BooksActivity extends AppCompatActivity {
                 BooksActivity.this.startActivity(intent);
             }
         });
+    }
+
+    private void setColors() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.statusBar));
+            window.setNavigationBarColor(ContextCompat.getColor(this, R.color.navigationBar));
+            ActionBar actionBar = this.getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.actionBar)));
+            }
+        }
     }
 }
--- app/src/main/java/arstulke/projectbook/LibrariesActivity.java
+++ app/src/main/java/arstulke/projectbook/LibrariesActivity.java
@@ -1,14 +1,26 @@
 package arstulke.projectbook;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.DragEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EdgeEffect;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
+import java.lang.reflect.Field;
+
+@SuppressWarnings("deprecation")
 public class LibrariesActivity extends AppCompatActivity {
 
     ListView listView;
@@ -17,12 +29,11 @@ public class LibrariesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_libraries);
-
         setTitle(getString(R.string.app_name) + ": Bibliotheken der BTC AG");
+        setColors();
 
-        String[] values = new String[] { "E5", "APW", "Hamburg", "Berlin"};
+        String[] values = new String[]{"E5", "APW", "Hamburg", "Berlin"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
         listView = (ListView) findViewById(R.id.librariesView);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -31,18 +42,55 @@ public class LibrariesActivity extends AppCompatActivity {
                 openBookList(position);
             }
         });
-        listView.setOnDragListener(new AdapterView.OnDragListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onDrag(View v, DragEvent event) {
-                return false;
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return showLibraryOptions(position);
             }
         });
     }
 
-    public void openBookList(int position){
+    private void setColors() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.statusBar));
+            window.setNavigationBarColor(ContextCompat.getColor(this, R.color.navigationBar));
+            ActionBar actionBar = this.getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.actionBar)));
+            }
+
+            
+        }
+    }
+
+    public void openBookList(int position) {
         String library = (String) listView.getItemAtPosition(position);
         Intent intent = new Intent(LibrariesActivity.this, BooksActivity.class);
         intent.putExtra("library", library);
         LibrariesActivity.this.startActivity(intent);
+    }
+
+    public boolean showLibraryOptions(int position) {
+        final String library = (String) listView.getItemAtPosition(position);
+        final String[] values = new String[]{"Bearbeiten", "Löschen"};
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Optionen: " + library);
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println(adapter.getItem(which));
+            }
+        });
+
+        AlertDialog ad = builder.create();
+        ad.show();
+
+        return true;
     }
 }
