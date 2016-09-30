1b31f934bc9d13d7649bc212f324b95978e75eb1
Stulken <Arne.Stulken@btc-ag.com>
2016-09-30 13:16:52 +0200

    Signed-off-by: Stulken <Arne.Stulken@btc-ag.com>

--- app/src/main/java/arstulke/projectbook/LibraryView.java
+++ app/src/main/java/arstulke/projectbook/LibraryView.java
@@ -2,15 +2,19 @@ package arstulke.projectbook;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LibraryView {
@@ -24,8 +28,9 @@ public class LibraryView {
         this.activity = activity;
     }
 
+    private ListView listView;
     public void show(List<String> bookList, RelativeLayout parent){
-        ListView listView = new ListView(applicationContext);
+        listView = new ListView(applicationContext);
         ArrayAdapter adapter = new ArrayAdapter<>(applicationContext, android.R.layout.simple_list_item_1, android.R.id.text1, bookList);
         listView.setAdapter(adapter);
         listView.setDivider(new ColorDrawable(resources.getColor(R.color.dividerColor)));
@@ -41,11 +46,29 @@ public class LibraryView {
         listView.setOnItemLongClickListener(new ListView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                throw new UnsupportedOperationException();
+            return showOptionsDialog(position);
             }
         });
 
         parent.addView(listView);
     }
 
+    private boolean showOptionsDialog(int position) {
+        ArrayList<String> options = new ArrayList<>();
+        Collections.addAll(options, "Bearbeiten", "Löschen");
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(applicationContext, android.R.layout.simple_list_item_1, android.R.id.text1, options);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(applicationContext);
+        builder.setTitle("Optionen: " + listView.getItemAtPosition(position));
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println(adapter.getItem(which));
+            }
+        });
+
+        AlertDialog ad = builder.create();
+        ad.show();
+        return true;
+    }
 }
