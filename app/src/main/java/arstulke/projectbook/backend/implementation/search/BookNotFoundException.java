65e530cf62cd1960fd7ba92360528e8d4a0599af
Stulken <Arne.Stulken@btc-ag.com>
2016-10-10 14:45:12 +0200

    Signed-off-by: Stulken <Arne.Stulken@btc-ag.com>

--- app/src/main/java/arstulke/projectbook/backend/implementation/search/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package arstulke.projectbook.backend.implementation.search;
-
-@SuppressWarnings({"WeakerAccess", "unused"})
-public class BookNotFoundException extends Exception {
-    public BookNotFoundException() { super(); }
-    public BookNotFoundException(String message) { super(message); }
-
-}
