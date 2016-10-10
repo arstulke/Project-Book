9537f8f748162d95512a4db2e445aec28b089387
Stulken <Arne.Stulken@btc-ag.com>
2016-10-10 09:02:48 +0200

    Signed-off-by: Stulken <Arne.Stulken@btc-ag.com>

--- app/src/test/java/arstulke/projectbook/RenderTester.java
+++ app/src/test/java/arstulke/projectbook/RenderTester.java
@@ -3,25 +3,29 @@ package arstulke.projectbook;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.List;
 
@@ -64,8 +68,8 @@ public class RenderTester {
         verify(parent).addView(listView);
     }
 
-    @SuppressWarnings("deprecation")
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Test
     public void openDefaultLayout() throws Exception {
         //given
         LibrariesActivity defaultActivity = mock(LibrariesActivity.class);
@@ -75,10 +79,10 @@ public class RenderTester {
         ActionBarDrawerToggle toggle = mock(ActionBarDrawerToggle.class);
         when(defaultActivity.findViewById(R.id.toolbar)).thenReturn(toolbar);
         when(defaultActivity.findViewById(R.id.drawer_layout)).thenReturn(drawer);
-        whenNew(ActionBarDrawerToggle.class).withArguments(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close).thenReturn(toggle);
+        whenNew(ActionBarDrawerToggle.class).withArguments(defaultActivity, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close).thenReturn(toggle);
 
         NavigationView navigationView = mock(NavigationView.class);
-        when(defaultActivity.findViewById(R.id.nav_view));
+        when(defaultActivity.findViewById(R.id.nav_view)).thenReturn(navigationView);
 
         //when
         defaultActivity.onCreate(null, null);
